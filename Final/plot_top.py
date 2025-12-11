5df721ee790638c49aa33d7b1bdc6a2784b6774c
ASachoTanzer <adin.sachotanzer@cooper.edu>
2025-12-11 18:46:17 -0500

    added top plotting, probably other misc stuff in final.py

---
+++ Final/plot_top.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 11 18:13:39 2025
+
+@author: Adin Sacho-Tanzer
+"""
+
+import numpy as np
+from matplotlib import pyplot as plt
+from inlet import inlet as Inlet
+
+def circle_top(xs, x_c, y_c, r):
+    return y_c+np.sqrt(r**2-(x_c-xs)**2)
+
+def plot_top(ax, inlet, x_offset, y_offset, x_end, top_thickness, inner_format="b", outer_format="k"):
+    """
+    plots top surface
+    Parameters
+    ax : matplotlip axis to plot on
+    inlet : inlet object
+    x_offset : x offset
+    y_offset: y offset
+    x_end : x to end at
+    top_thickness : thickness of top surface
+    inner_format : format code for inner surface. The default is "b".
+    outer_format : format code for outer surface. The default is "k".
+    """
+    x_c = inlet.xs[-1]
+    y_c = inlet.ys[-1]
+    r = np.sqrt((inlet.xs[-1]-inlet.x_lip)**2+(inlet.ys[-1]-inlet.y_lip)**2)
+    
+    xs_circle = np.linspace(inlet.x_lip, x_c,)
+    ys_circle = circle_top(xs_circle, x_c, y_c, r)
+    
+    ax.plot(xs_circle+x_offset, ys_circle+y_offset, inner_format)
+    ax.plot([xs_circle[-1]+x_offset, x_end+x_offset], [ys_circle[-1]+y_offset, ys_circle[-1]+y_offset], inner_format)
+    
+    slope = (inlet.ys[-1]-inlet.ys[-2])/(inlet.xs[-1]-inlet.xs[-2])
+    ax.plot([inlet.x_lip+x_offset, x_c+x_offset, x_end+x_offset], [inlet.y_lip+y_offset, inlet.y_lip+slope*(x_c-inlet.x_lip)+y_offset, inlet.y_lip+slope*(x_c-inlet.x_lip)+y_offset], outer_format)
+ 
+    
+if __name__ == "__main__":
+     P_atm = 9112.32  # Pa
+     T_atm = 216.65  # K
+     M_atms = np.linspace(2.75, 3.25)
+     M_atms = [3.25]
+    
+     # Basic properties:
+     m_dot = 10  # kg/s
+     width = 1  # m
+    
+     # Inlet:
+     M_max = M_atms[-1]
+     turn_angles = [10, 10, 10]  # turn angles of inlet, degrees
+     inlet = Inlet(P_atm, T_atm, M_max, m_dot, turn_angles, width=width)
+    
+     ax = plt.subplot()
+     plot_top(ax, inlet, 0, 0, 1, 0.005)
+     ax.set_aspect("equal")
+     plt.xlim(0.1, 0.2)
+     plt.ylim(0.05, 0.1)
+     plt.show()
